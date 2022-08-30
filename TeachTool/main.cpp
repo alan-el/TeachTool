e2bf4963dbe92d836a82997122fda6f175d0b051
Alan Zhang <zqn_1996@163.com>
2022-08-30 19:40:05 +0800

    Add yolo object detectation

--- TeachTool/main.cpp
+++ TeachTool/main.cpp
@@ -1,10 +1,15 @@
 ﻿#include "stdafx.h"
 #include <QtWidgets>
 #include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
 #include "teachtool.h"
 #include "liblouis.h"
-using namespace std;
+#include "yolo.h"
+//using namespace cv;
+
+extern Net_config yolo_nets[4];
 
 int main(int argc, char *argv[])
 {
@@ -17,10 +22,21 @@ int main(int argc, char *argv[])
 	int ret = lou_translateString("D:/MyProject/BlinderReader/software/Chapter5/liblouis/liblouis-3.22.0-win64/share/liblouis/tables/zhcn-g1.ctb", in, &in_len, out, &out_len, NULL, space, dotsIO);
 	cout << ret << endl;*/
 	
-    QApplication a(argc, argv);
+	/**/YOLO yolo_model(yolo_nets[2]);
+	string imgpath = "slide3_pict2.jpg";
+	Mat srcimg = imread(imgpath);
+	yolo_model.detect(srcimg);
+
+	static const string kWinName = "Deep learning object detection in OpenCV";
+	namedWindow(kWinName, WINDOW_NORMAL);
+	imshow(kWinName, srcimg);
+	waitKey(0);
+	destroyAllWindows();
+
+    /*QApplication a(argc, argv);
     TeachTool w;
     w.show();
-    return a.exec();
+    return a.exec();*/
 }
 
 
